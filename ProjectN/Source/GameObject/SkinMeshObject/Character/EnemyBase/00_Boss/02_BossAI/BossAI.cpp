aecffb93a7352fcc218d11d533e37d7c0d0c2241
game <game@R200-18>
2025-12-18 16:41:32 +0900

    敵のAIの作成中

---
+++ ProjectN/Source/GameObject/SkinMeshObject/Character/EnemyBase/00_Boss/02_BossAI/BossAI.cpp
@@ -0,0 +1,24 @@
+#include "BossAI.h"
+
+#include "GameObject//StaticMeshObject//Portal//Portal.h"
+
+BossAI::BossAI()
+{
+}
+
+BossAI::~BossAI()
+{
+}
+
+void BossAI::Update()
+{
+}
+
+void BossAI::AIUpdate()
+{
+}
+
+//1. —Dæ“x1‚Ìˆ—‚ğ‘‚­ŠÖ”.
+void BossAI::PortalToBoss()
+{
+}
