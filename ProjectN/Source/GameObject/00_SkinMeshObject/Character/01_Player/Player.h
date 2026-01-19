8304143114eab9d23b28b431e941368d304e76ad
game <game@R200-18>
2026-01-19 16:10:39 +0900

    Playerクラス修正

--- ProjectN/Source/GameObject/00_SkinMeshObject/Character/01_Player/Player.h
+++ ProjectN/Source/GameObject/00_SkinMeshObject/Character/01_Player/Player.h
@@ -1,28 +1,37 @@
 #pragma once
 #include "GameObject/00_SkinMeshObject/Character/Character.h"
 
+//メッシュをアタッチする.
+class SkinMeshManager;
+//プレイヤーの弾マネージャークラス.
 class PShotManager;
 class Timer;
+//プレイヤーのステートクラス.
 class PlayerState;
+//待機クラス.
 class PlayerIdol;
+//アニメーションの設を呼ぶクラス.
 class PlayerContext;
+//死んだとき.
 class PlayerDead;
 
 //ポータル取得時に再生するアニメーションのクラス.
 class PlayerPortalAnim;
+class Shadow;
 
 #include "System/00_Manager/07_PlayerAttackManager/PlayerAttackManager.h"
 #include "..//01_Player/PlayerAttckStateBase/AttackShort/AttackShort.h"
 
-#include "GameObject/01_SpriteObject/00_Shadow/Shadow.h"
 
 /**************************************************
 *	プレイヤークラス.
+*	ToDo : Characterクラスを親として継承関係を結ぶ.
 **/
 class Player final
 	: public Character
 {
 public:
+	//フレンドを結んでいる.
 	friend class PlayerContext;
 public:
 	Player();
@@ -33,15 +42,12 @@ public:
 	void Init() override;
 
 	void Hit();
-
-	void SetAnimSpeed(double speed) { m_AnimSpeed = speed; }
-
 public:
 
 	BoundingSphere& GetBoundingSphere() { return m_BSphere; }
 
 	PShotManager* GetShotManager() { return m_pShotManager; }
-
+	//当たり判定の位置を設定.
 	D3DXVECTOR3 GetHitCenter() const;
 
 	//HPを取得する関数.
@@ -49,10 +55,12 @@ public:
 	// Playerの動作の初期化用関数.
 	// 攻撃マネージャークラスの遠距離攻撃の初期化.
 	// PlayerIdolの初期化.
-	// 他の方法で作成するやり方を調べて実装する.
 	void InitializePlayerMove();
-
+	
+	//ToDo : プレイヤーが死亡した際に呼ばれる関数.
+	//復活関数.
 	void Respawn();
+	//死んだ状態取得関数.
 	bool IsDead() const { return m_pCurrentState == (PlayerState*)m_pPlayerDead.get(); }
 
 public:
@@ -62,31 +70,34 @@ public:
 	//A・Dの左右用関数.
 	D3DXVECTOR3 Player_AD(float RotationY) const;
 
+	//攻撃ステートの切り替え関数.
+	//ToDo : PlayerAttackManagerに遷移タイミングは任している.
 	void ChangeAttackType(PlayerAttackManager::enAttack type);
-	void CleanUpAttackState(PlayerAttackManager::enAttack type);
 
 	// ボーンの位置を取得する関数
 	bool GetBonePosition(const char* boneName, D3DXVECTOR3* outPos) const;
 
+	//プレイヤーの遠距離攻撃関数.
+	//ToDo : 遠距離攻撃を発射する座標を設定している.
 	D3DXVECTOR3 GetShortAttackCenter() const;
+	//.
 	AttackShort* GetShortAttackState() const;
 
 	PlayerAttackManager* GetAttackManager() const { return m_pAttackManager.get(); }
 public:
-	//ポータル系の関数をここに書く.
 	//PortalからStateを切り替えるため.
 	//セットキャプチャステート関数.
 	void SetCaptureState(float duration);
 	//Portalがゲージ増加停止を判定するための関数.
 	bool IsCapturingState() const;
 
-	Shadow* GetShadow() const { return m_pShadow.get(); }
+	Shadow* GetShadow() const		{ return m_pShadow.get(); }
 
-	void SetVisible(bool visible) { m_IsVisible = visible; }
-	bool IsVisible() const { return m_IsVisible; }
+	void SetVisible(bool visible)	{ m_IsVisible = visible; }
+	bool IsVisible() const			{ return m_IsVisible; }
 
-	void SetFrozen(bool frozen) { m_IsFrozen = frozen; }
-	bool IsFrozen() const { return m_IsFrozen; }
+	void SetFrozen(bool frozen)		{ m_IsFrozen = frozen; }
+	bool IsFrozen() const			{ return m_IsFrozen; }
 private:
 	PShotManager*	m_pShotManager;
 	std::unique_ptr<PlayerAttackManager> m_pAttackManager;
